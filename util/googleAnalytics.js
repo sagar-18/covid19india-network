819e0394f641da48733718af5e8c7f8d80862dfe
Sagar Chauhan <36581523+sagar-18@users.noreply.github.com>
2020-03-25 09:25:00 +0530

    Update googleAnalytics.js

--- util/googleAnalytics.js
+++ util/googleAnalytics.js
@@ -1,10 +1,11 @@
 import ReactGA from 'react-ga'
 
 export const initGA = () => {
-  ReactGA.initialize('UA-161578421-2')
+  ReactGA.initialize('UA-161578421-4')
 }
 
 export const logPageView = () => {
   ReactGA.set({ page: window.location.pathname })
   ReactGA.pageview(window.location.pathname)
 }
+
